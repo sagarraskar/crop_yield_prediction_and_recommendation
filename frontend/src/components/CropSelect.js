27aeb5c6c340e7c42998f26f5d058d160b0b865f
Adi190920 <adiabhang04@gmail.com>
2022-03-01 14:18:51 +0530

    Districts, state and crops added

--- frontend/src/components/CropSelect.js
+++ frontend/src/components/CropSelect.js
@@ -19,4 +19,5 @@ export default function StateSelect({ crop, setCrop }) {
   );
 }
 
-const crops =  ['Maize', 'Wheat']
+const crops =  ['Beans & Mutter(Vegetable)', 'Cabbage', 'Drum Stick', 'Pulses total', 'Cauliflower', 'Total foodgrain', 'Bhindi', 'Tomato', 'Ginger', 'Orange', 'Pome Fruit', 'Citrus Fruit', 'Pineapple', 'Brinjal', 'Other Cereals & Millets', 'Other Vegetables', 'Cardamom', 'Mango', 'Other Fresh Fruits', 'Cowpea(Lobia)', 'Papaya', 'Moth', 'other oilseeds', 'Paddy', 'Guar seed', 'Safflower', 'Cashewnut', 'Black pepper', 'Khesari', 'Jute', 'Arecanut', 'Coconut ', 'Niger seed', 'Mesta', 'Tapioca', 'Sannhamp', 'Tobacco', 'Soyabean', 'Castor seed', 'Other  Rabi pulses', 'Dry ginger', 'Other Kharif pulses', 'Garlic', 'Banana', 'Coriander', 'Cotton(lint)', 'Horse-gram', 'Bajra', 'Ragi', 'Turmeric', 'Barley', 'Masoor', 'Sunflower', 'Linseed', 'Small millets', 'Sweet potato', 'Peas & beans (Pulses)', 'Jowar', 'Dry chillies', 'Onion', 'Gram', 'Potato', 'Groundnut', 'Arhar/Tur', 'Wheat', 'Sugarcane', 'Rapeseed &Mustard', 'Sesamum', 'Urad', 'Moong(Green Gram)', 'Maize', 'Rice']
+
