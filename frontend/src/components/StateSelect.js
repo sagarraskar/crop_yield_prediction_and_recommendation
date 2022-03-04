98c3b0d70c051a9cea963cdd13ff986d1aa45b9a
Adi190920 <adiabhang04@gmail.com>
2022-03-04 11:52:30 +0530

    frontend : state and district list updated

--- frontend/src/components/StateSelect.js
+++ frontend/src/components/StateSelect.js
@@ -20,9 +20,11 @@ export default function StateSelect({ state, setState }) {
   );
 }
 
-const states = ['Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh',
-'Dadra and Nagar Haveli', 'Goa', 'Haryana', 'Himachal Pradesh',
-'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra',
- 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry',
-'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Tripura',
-'Uttar Pradesh', 'Uttarakhand'];
+const states = ['Andaman and Nicobar Islands', 'Andhra Pradesh',
+       'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh',
+       'Chhattisgarh', 'Dadra and Nagar Haveli', 'Goa', 'Gujarat',
+       'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir ', 'Jharkhand',
+       'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur',
+       'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry',
+       'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana ',
+       'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'];
